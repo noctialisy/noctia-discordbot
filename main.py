38b4df77e90de6b6aaffbc6ae071e6416ecb83c5
noctialisy <1392499+noctialisy@users.noreply.github.com>
2026-01-08 20:32:05 +0100

    Fix raw stat assign reporting the wrong stat as assigned

--- main.py
+++ main.py
@@ -219,9 +219,12 @@ with open('./settings.json', 'r', encoding='utf-8') as settings_file:
 
         # Character existence check
         if character != "Character not found or not loaded":
-            character.set_raw_stat(raw_stat_index, character_stat)
-            GameSystem.save_character(user_id, character)
-            await ctx.response.send_message("RAW Stat " + str(character.get_raw_stats()[raw_stat_index]) + " Assigned to " + character_stat, ephemeral=True)
+            char_raw_stats = character.get_raw_stats()
+
+            if 0 < raw_stat_index < len(char_raw_stats):
+                character.set_raw_stat(raw_stat_index, character_stat)
+                GameSystem.save_character(user_id, character)
+                await ctx.response.send_message("RAW Stat " + str(char_raw_stats[raw_stat_index]) + " Assigned to " + character_stat, ephemeral=True)
 
         else:
             await ctx.response.send_message("You don't have a RP character currently.", ephemeral=True)
